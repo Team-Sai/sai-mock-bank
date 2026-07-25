8aab4a316359374ef64ec512c744daf688a8ec62
yeonchaepark <check4732@naver.com>
2026-07-26 02:47:28 +0900

    Initial commit

---
+++ src/main/java/org/example/saimockbank/SaiMockBankApplication.java
@@ -0,0 +1,13 @@
+package org.example.saimockbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiMockBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaiMockBankApplication.class, args);
+    }
+
+}
